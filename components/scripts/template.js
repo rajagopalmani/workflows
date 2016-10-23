78d6948425e5d67081de3fafb2205ffedf3a6b22
Raja Gopal <rajagopalmani5@yahoo.com>
2016-10-23 14:18:07 +0530

    First Commit

---
+++ components/scripts/template.js
@@ -0,0 +1,9 @@
+/**global '$'*/
+$(function () {
+    var Mustache = require('mustache');
+    $.getJSON('js/data.json', function (data) {
+        var template = $('#speakerstpl').html();
+        var html = Mustache.to_html(template, data);
+        $('#speakers').html(html);
+    }); //getJSON
+}); //function
