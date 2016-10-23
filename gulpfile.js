6d21660519e6733a9f6d7b20b92f248fe705dd42
Raja Gopal <rajagopalmani5@yahoo.com>
2016-10-23 19:12:07 +0530

    compass installed

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    compass = require('gulp-compass');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = ['components/scripts/pixgrid.js',
                 'components/scripts/rclick.js',
                 'components/scripts/tagline.js',
                 'components/scripts/template.js'];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
@@ -20,9 +22,20 @@ gulp.task('coffee', function () {
         .pipe(gulp.dest('components/scripts'))
 })
 
-gulp.task('concat', function () {
+gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('css', function () {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded' //compressed for prod
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'))
 });
