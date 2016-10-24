1b5e58f0bb63be1fe47322a1885795fb83638580
Raja Gopal <rajagopalmani5@yahoo.com>
2016-10-25 01:33:21 +0530

    Added environment variable detection

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,29 @@ var gulp = require('gulp'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = ['components/scripts/rclick.js',
-                 'components/scripts/pixgrid.js',
-                'components/scripts/tagline.js',
-                'components/scripts/template.js'];
-var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/index.html'];
-var jsonSources = ['builds/development/js/*.json'];
+var env, coffeeSources, jsSources, sassSources, htmlSources, jsonSources, outputDir, sassStyle;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    outputDir = 'builds/development/';
+    sassStyle = 'expanded';
+} else {
+    outputDir = 'builds/production/';
+    sassStyle = 'compressed';
+}
+
+coffeeSources = ['components/coffee/tagline.coffee'];
+jsSources = [
+            'components/scripts/rclick.js',
+            'components/scripts/pixgrid.js',
+            'components/scripts/tagline.js',
+            'components/scripts/template.js'
+            ];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + 'index.html'];
+jsonSources = [outputDir + 'js/*.json'];
+
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
         .pipe(coffee({
@@ -28,7 +43,7 @@ gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
-        .pipe(gulp.dest('builds/development/js'))
+        .pipe(gulp.dest(outputDir + 'js'))
         .pipe(connect.reload());
 });
 
@@ -36,11 +51,11 @@ gulp.task('css', function () {
     gulp.src(sassSources)
         .pipe(compass({
             sass: 'components/sass',
-            image: 'builds/development/images',
-            style: 'expanded' //compressed for prod
+            image: outputDir + 'images',
+            style: 'compressed'
         }))
         .on('error', gutil.log)
-        .pipe(gulp.dest('builds/development/css'))
+        .pipe(gulp.dest(outputDir + 'css'))
         .pipe(connect.reload());
 });
 
@@ -64,7 +79,7 @@ gulp.task('watch', function () {
 
 gulp.task('connect', function () {
     connect.server({
-        root: 'builds/development/',
+        root: outputDir,
         livereload: true
     })
 });
